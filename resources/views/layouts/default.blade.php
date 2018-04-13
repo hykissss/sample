eca7aef48fc2a9781d70995365da4a2a36c4f904
k155 <hy_kissss@163.com>
2018-04-13 15:35:23 +0000

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,11 +2,21 @@
 <html>
   <head>
     <!--第一个参数是该区块的变量名称，第二个参数是当指定变量值为空值时使用的默认值-->
-    <title>@yield('title', 'Sample') - Laravel新手入门教程</title>
+    <title>@yield('title', 'Sample') - Laravel入门教程</title>
+    <link rel="stylesheet" href="/css/app.css">
   </head>
+
   <body>
-    <!--表示该占位区域将用于显示content区块的内容，而content区块的内容将由继承自default视图的子视图定义-->
-    @yield('content')
+    @include('layouts._header')
+
+    <div class="container">
+        <div class="col-md-offset-1 col-md-10">
+        <!--表示该占位区域将用于显示content区块的内容，而content区块的内容将由继承自default视图的子视图定义-->
+            @yield('content')
+            @include('layouts._footer')
+        </div>
+    </div>
+
   </body>
 
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,7 +1,20 @@
 @extends('layouts.default')
 
+<!--所有包含在@ section和@ stop中的代码都将被插入到父视图的content区块-->
 @section('content')
-<h1>主页</h1>
+
+<div class="jumbotron">
+    <h1>Hello Laravel</h1>
+    <p class="lead">
+        你现在看到的是 <a href="https://laravel-china.org/courses/laravel-essential-training-5.1">Laravel 入门教程</a> 的示例项目主页。
+    </p>
+    <p>
+        一切，将从这里开始
+    </p>
+    <p>
+        <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
+    </p>
+</div>
+
 @stop
 
-<!--所有包含在@ section和@ stop中的代码都将被插入到父视图的content区块-->
