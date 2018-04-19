239ed69e555e9341afb11cfb15a92f5469e0ef26
k155 <hy_kissss@163.com>
2018-04-19 02:21:20 +0000

    Add user statuses

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -2,19 +2,33 @@
 
 <!--所有包含在@ section和@ stop中的代码都将被插入到父视图的content区块-->
 @section('content')
-
-<div class="jumbotron">
-    <h1>Hello Laravel</h1>
-    <p class="lead">
-        你现在看到的是 <a href="https://laravel-china.org/courses/laravel-essential-training-5.1">Laravel 入门教程</a> 的示例项目主页。
-    </p>
-    <p>
-        一切，将从这里开始
-    </p>
-    <p>
+  @if (Auth::check())
+    <div class="row">
+      <div class="col-md-8">
+        <section class="status_form">
+          @include('shared._status_form')
+        </section>
+        <h3>微博列表</h3>
+        @include('shared._feed')
+      </div>
+      <aside class="col-md-4">
+        <section class="user_info">
+          @include('shared._user_info', ['user' => Auth::user()])
+        </section>
+      </aside>
+    </div>
+  @else
+    <div class="jumbotron">
+      <h1>Hello Laravel</h1>
+      <p class="lead">
+        你现在所看到的是 <a href="https://laravel-china.org/laravel-tutorial/5.1">Laravel 入门教程</a> 的项目主页。
+      </p>
+      <p>
+        一切，将从这里开始。
+      </p>
+      <p>
         <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
-    </p>
-</div>
-
+      </p>
+    </div>
+  @endif
 @stop
-
